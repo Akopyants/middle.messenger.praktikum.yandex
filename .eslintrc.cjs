9a07f2f7a9625de6c877a7accb3cdd1e0ab4e886
Akopyants <akopyants92@yandex.ru>
2024-03-27 13:15:27 +0300

    upd

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
+    env: {
+      browser: true,
+      es2021: true,
     },
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {},
-  extends: ['airbnb-typescript'],
-};
+    overrides: [
+      {
+        env: {
+          node: true,
+        },
+        files: ['.eslintrc.{js,cjs}'],
+        parserOptions: {
+          sourceType: 'script',
+        },
+      },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 'latest',
+      project: './tsconfig.json',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {},
+    extends: ['airbnb-typescript'],
+  };
+  
