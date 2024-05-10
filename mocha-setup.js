15b30c273b173cb0d6e26a0a022895c998efcc8e
Akopyants <akopyants92@yandex.ru>
2024-05-10 19:26:27 +0300

    upd

--- mocha-setup.js
+++ mocha-setup.js
@@ -5,5 +5,3 @@ const dom = new JSDOM('<div id="app"></div>', { url: 'http://localhost:3000' });
 global.window = dom.window;
 global.document = window.document;
 global.XMLHttpRequest = window.XMLHttpRequest;
-
-
